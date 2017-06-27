4b3a3b3c0ff01b268c705ab0b4dc58735e8925e9
aideslucas <aideslucas@gmail.com>
2017-06-27 10:07:29 +0300

    Add files via upload

---
+++ Camera_Viewer_Connect_USB_WF/MainForm.Designer.cs
@@ -0,0 +1,110 @@
+﻿namespace Camera_Viewer_Connect_USB_WF
+{
+    partial class MainForm
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.connectBt = new System.Windows.Forms.Button();
+            this.disconnectBt = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.liveViewer = new Ozeki.Media.VideoViewerWF();
+            this.txtDecodedData = new System.Windows.Forms.TextBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // connectBt
+            // 
+            this.connectBt.Location = new System.Drawing.Point(12, 9);
+            this.connectBt.Name = "connectBt";
+            this.connectBt.Size = new System.Drawing.Size(131, 23);
+            this.connectBt.TabIndex = 0;
+            this.connectBt.Text = "Connect to web camera";
+            this.connectBt.UseVisualStyleBackColor = true;
+            this.connectBt.Click += new System.EventHandler(this.connectBt_Click);
+            // 
+            // disconnectBt
+            // 
+            this.disconnectBt.Enabled = false;
+            this.disconnectBt.Location = new System.Drawing.Point(261, 9);
+            this.disconnectBt.Name = "disconnectBt";
+            this.disconnectBt.Size = new System.Drawing.Size(169, 23);
+            this.disconnectBt.TabIndex = 2;
+            this.disconnectBt.Text = "Disconnect from web camera";
+            this.disconnectBt.UseVisualStyleBackColor = true;
+            this.disconnectBt.Click += new System.EventHandler(this.disconnectBt_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.liveViewer);
+            this.groupBox1.Location = new System.Drawing.Point(12, 42);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(424, 428);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Live";
+            // 
+            // liveViewer
+            // 
+            this.liveViewer.BackColor = System.Drawing.Color.Black;
+            this.liveViewer.ContextMenuEnabled = true;
+            this.liveViewer.FlipMode = Ozeki.Media.FlipMode.None;
+            this.liveViewer.FrameStretch = Ozeki.Media.FrameStretch.Uniform;
+            this.liveViewer.FullScreenEnabled = true;
+            this.liveViewer.Location = new System.Drawing.Point(6, 19);
+            this.liveViewer.Name = "liveViewer";
+            this.liveViewer.RotateAngle = 0;
+            this.liveViewer.Size = new System.Drawing.Size(412, 403);
+            this.liveViewer.TabIndex = 0;
+            this.liveViewer.Text = "liveViewer";
+            // 
+            // txtDecodedData
+            // 
+            this.txtDecodedData.Location = new System.Drawing.Point(12, 476);
+            this.txtDecodedData.Multiline = true;
+            this.txtDecodedData.Name = "txtDecodedData";
+            this.txtDecodedData.Size = new System.Drawing.Size(424, 108);
+            this.txtDecodedData.TabIndex = 10;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(445, 596);
+            this.Controls.Add(this.txtDecodedData);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.disconnectBt);
+            this.Controls.Add(this.connectBt);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainForm";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Frame capture";
+            this.groupBox1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connectBt;
+        private System.Windows.Forms.Button disconnectBt;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private Ozeki.Media.VideoViewerWF liveViewer;
+        private System.Windows.Forms.TextBox txtDecodedData;
+    }
+}
---
+++ Camera_Viewer_Connect_USB_WF/MainForm.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Windows.Forms;
+using Ozeki.Camera;
+using Ozeki.Media;
+using System.Drawing;
+using ZXing;
+
+namespace Camera_Viewer_Connect_USB_WF
+{
+    public partial class MainForm : Form
+    {
+        private FrameCapture _capture;
+
+        private WebCamera _webCamera;
+
+        private MediaConnector _connector;
+
+        private DrawingImageProvider _liveProvider;
+
+        private VideoResizer _resizeHandler;
+        
+        public MainForm()
+        {
+            InitializeComponent();
+            _liveProvider = new DrawingImageProvider();
+            _resizeHandler = new VideoResizer();
+
+            _capture = new FrameCapture();
+            _connector = new MediaConnector();
+        }
+
+        private void connectBt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                disconnectBt.Enabled = true;
+
+                _webCamera = new WebCamera();
+
+                _connector.Connect(_webCamera, _liveProvider);
+
+                _connector.Connect(_webCamera, _capture);
+
+                _connector.Connect(_webCamera.VideoChannel, _resizeHandler);
+
+                _resizeHandler.SetResolution(600, 800);
+
+                liveViewer.SetImageProvider(_liveProvider);
+                liveViewer.Start();
+
+                
+
+                _webCamera.Start();
+
+                start();
+
+                connectBt.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void disconnectBt_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                stop();
+
+                _capture.Stop();
+                _webCamera.Stop();
+
+                _connector.Disconnect(_webCamera, _liveProvider);
+
+                _connector.Disconnect(_webCamera, _capture);
+
+                liveViewer.Stop();
+
+                _capture.Dispose();
+
+
+                connectBt.Enabled = true;
+                disconnectBt.Enabled = false;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void start()
+        {
+            try
+            {
+                _capture.SetInterval(new TimeSpan(0, 0, 0, 0, 500));
+                _capture.Start();
+                _capture.OnFrameCaptured += _capture_OnFrameCaptured;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        void _capture_OnFrameCaptured(object sender, Snapshot e)
+        {
+            var image = e.ToImage();
+
+            try
+            {
+                BarcodeReader reader = new BarcodeReader { AutoRotate = true, TryHarder = true };
+                reader.Options.TryHarder = true;
+                Result result = reader.Decode(new Bitmap(image));
+                if (result.Text.Contains("[{"))
+                {
+                    txtDecodedData.Text = result.Text;
+                    stop();
+                    disconnectBt_Click(null, null);
+                }
+            }
+            catch (Exception ex)
+            {
+                txtDecodedData.Text += "Error: " + (ex.Message) + " \r\n\r\n";
+            }
+        }
+
+        private void stop()
+        {
+            _capture.Stop();
+            _capture.OnFrameCaptured -= _capture_OnFrameCaptured;
+        }
+    }
+}
